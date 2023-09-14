d04f3b30b61ba8557dc3255aa98e1177ec41178c
Zunair <zunair.dev@gmail.com>
2023-09-15 01:58:26 +0500

    slider: added the association with validations

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -1,2 +1,7 @@
 class Slider < ApplicationRecord
+  has_one_attached :background_image
+  has_one_attached :logo
+
+  validates :background_image, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  validates :logo, content_type: ['image/png', 'image/jpg', 'image/jpeg']
 end
