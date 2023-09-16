83aeaa1495c6805da07563ea360552743282934a
Zunair <zunair.dev@gmail.com>
2023-09-17 04:36:01 +0500

    rails_downgrade: downgraded rails 7 to rails 6

--- db/migrate/20230916130404_create_services.rb
+++ db/migrate/20230916130404_create_services.rb
@@ -1,4 +1,4 @@
-class CreateServices < ActiveRecord::Migration[7.0]
+class CreateServices < ActiveRecord::Migration[6.1]
   def change
     create_table :services do |t|
       t.string :name
--- db/migrate/20230916214608_create_administrators.rb
+++ db/migrate/20230916214608_create_administrators.rb
@@ -1,4 +1,4 @@
-class CreateAdministrators < ActiveRecord::Migration[7.0]
+class CreateAdministrators < ActiveRecord::Migration[6.1]
   def change
     create_table :administrators do |t|
       t.string :email
