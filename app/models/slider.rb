f25ca0083f487a6029d19f51da2b6edfac29bf9d
Zunair <zunair.dev@gmail.com>
2023-09-16 18:01:30 +0500

    slider: added slider model

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -1,7 +1,4 @@
 class Slider < ApplicationRecord
-  has_one_attached :background_image
-  has_one_attached :logo
-
-  validates :background_image, content_type: ['image/png', 'image/jpg', 'image/jpeg']
-  validates :logo, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  # has_one_attached :background_image
+  # has_one_attached :logo
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_14_204430) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_205018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_14_204430) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "sliders", force: :cascade do |t|
+    t.string "header"
+    t.string "title"
+    t.string "context"
+    t.text "description"
+    t.string "link_text"
+    t.string "link_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "background_image"
+    t.string "logo"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
