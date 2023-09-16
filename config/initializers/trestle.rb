3724b66a6c339765c94f539868f61976a355b6d4
Zunair <zunair.dev@gmail.com>
2023-09-17 02:59:41 +0500

    admin_auth: updated the app name on the admin side

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -3,7 +3,7 @@ Trestle.configure do |config|
   #
   # Set the page title shown in the main header within the admin.
   #
-  config.site_title = "Islam"
+  config.site_title = "Imam Sidyot"
 
   # Specify a custom image to be used in place of the site title for mobile and
   # expanded/desktop navigation. These images should be placed within your
