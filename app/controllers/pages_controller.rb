8f1ff9d9d430fcf9d345182987695b1858c9f0a4
Zunair <zunair.dev@gmail.com>
2023-09-18 02:44:35 +0500

    about_section: made the about section dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
     @sliders = Slider.all
+    @about = About.first
   end
 end
--- app/models/about.rb
+++ app/models/about.rb
@@ -1,3 +1,14 @@
 class About < ApplicationRecord
-  has_one_attached :images
+  has_many_attached :images
+
+  before_save :check_existing_entry
+
+  private
+
+  def check_existing_entry
+    if About.exists?
+      errors.add(:base, 'Only one About entry is allowed')
+      throw(:abort)
+    end
+  end
 end
