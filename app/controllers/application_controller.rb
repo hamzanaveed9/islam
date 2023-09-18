db897d0ec0e410902b1290806782127e33d43d86
umairr.azam <omairrr.azam@gmail.com>
2023-09-18 21:44:19 +0500

    feat:header footer functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :load_header_setting
+
+  private
+
+  def load_header_setting
+    @header_setting = HeaderSetting.last
+  end
 end
