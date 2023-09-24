e28e22813284a1fee0f43a2abdff400e1fa3ff82
Zunair <zunair.dev@gmail.com>
2023-09-24 19:33:34 +0500

    feedbacks: updated the feedback form

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,7 @@
 class ServicesController < ApplicationController
   def show
     @service = Service.find(params[:id])
+    @feedbacks = @service.feedbacks
     @feedback = @service.feedbacks.new
   end
 end
