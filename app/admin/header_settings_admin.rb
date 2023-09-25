af90f2e32e6413c027595c16dd7d2939bcdd6ff4
Zunair <zunair.dev@gmail.com>
2023-09-25 19:10:19 +0500

    contacts: added 3 contacts to the pages

--- app/admin/header_settings_admin.rb
+++ app/admin/header_settings_admin.rb
@@ -3,6 +3,16 @@ Trestle.resource(:header_settings) do
     item "Settings", icon: "fa fa-solid fa-cog"
   end
 
+  table do
+    column :id
+    column :tagline
+    column :menu_order
+    column :footer_description
+    column :office_working_days
+    column :timings
+    actions
+  end
+
   form do |header_setting|
     tab :general do
       file_field :logo_image
@@ -20,12 +30,6 @@ Trestle.resource(:header_settings) do
     tab :office do
       text_field :office_working_days,label:'Weekdays timings', placeholder: "Weekdays timings e.g., 8:00 am - 6:00 pm"
       text_field :timings, label:'Weekend timings', placeholder: "Weekend timings e.g., 8:00 am - 6:00 pm"
-    end
-
-    tab :contact do
-      text_field :phone1, placeholder: "Phone 1"
-      text_field :phone2, placeholder: "Phone 2"
-      text_field :email, placeholder: "Email"
     end
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,12 @@ class PagesController < ApplicationController
     @latest_event = Event.order(:date).first
     @services = Service.all
     @feedback = Feedback.new
+    @contacts = Contact.all
   end
 
   def about_us
     @about = About.first
     @feedback = Feedback.new
+    @contacts = Contact.all
   end
 end
